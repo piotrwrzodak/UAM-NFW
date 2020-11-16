1418d3695e7875a0be945cd4f6fc996becbd97e4
piotrwrzodak <piotrwrzodakk@gmail.com>
2020-11-16 14:00:54 +0100

    zajecia 2 - state, hooks, fetch

--- apps/nfw-react/src/app/app.tsx
+++ apps/nfw-react/src/app/app.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-// import { Zajecia2 } from './zajecia2';
-// import { Zajecia3 } from './zajecia3';
+import { Zajecia2 } from './zajecia2';
+import { Zajecia3 } from './zajecia3';
 import { Zajecia4 } from './zajecia4';
 import { Zajecia5 } from './zajecia5';
 
 export const App = () => {
   return (
-    // <Zajecia2 />
+    <Zajecia2 />
     // <Zajecia3 />
-    <Zajecia4 />
+    // <Zajecia4 />
     // <Zajecia5 />
   );
 };
