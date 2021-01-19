1dfa0af2fb305f8da46b93c7faa97dc5c9cc892a
piotrwrzodak <piotrwrzodakk@gmail.com>
2021-01-19 18:55:37 +0100

    add styles to cart & modify menu styles

--- apps/nfw-react/src/app/project/cmp/Cart/Cart.cmp.jsx
+++ apps/nfw-react/src/app/project/cmp/Cart/Cart.cmp.jsx
@@ -1,3 +1,17 @@
 import React from 'react';
+import { Nav } from '../Nav';
 
-export const Cart = () => <h1>cart</h1>;
+export const Cart = () => {
+  return (
+    <>
+      <Nav />
+
+      <div className="cart">
+        <h1 className="cart__heading">Your order:</h1>
+        <ul className="cart__list">
+          <li className="cart__item"></li>
+        </ul>
+      </div>
+    </>
+  );
+};
